f27e58b13dbf8e8ebb8a7cb875ef197708c7efb1
weliton <welingtonzsilva@gmail.com>
2021-12-01 01:28:07 -0300

    estudos javascript

---
+++ objeto/objetos.js
@@ -0,0 +1,8 @@
+const listaCpf = ['36544477710','12345678910','22233344478'];
+
+const cliente = {
+    nome:"andré",
+    idade:36,
+    cpf:"12345678910",
+    email:"andre@email.com"
+}
---
+++ objeto/print-info.js
@@ -0,0 +1,8 @@
+const cliente = {
+    nome:"andré",
+    idade:36,
+    cpf:"12345678910",
+    email:"andre@email.com"
+}
+
+console.log(cliente.idade)
